1dced70f6dab1d920cd97ff7ced42c0f1397fc66
Mokrov Danila <d.mokrov@norsi-trans.ru>
2025-09-16 02:20:52 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// Переключение вкладок преимуществ
+import './styles/style.scss';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+})
+  
